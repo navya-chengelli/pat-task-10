5cf667108644af27c19b97af98235b8ef04e0043
navya-chengelli <160773093+navya-chengelli@users.noreply.github.com>
2024-05-26 13:10:38 +0530

    Add files via upload

--- TASK-10.py
+++ TASK-10.py
@@ -1,3 +1,5 @@
+
+
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -14,16 +16,16 @@ driver = webdriver.Chrome(options=chrome_options)
 # Navigate to the Instagram profile
 driver.get("https://www.instagram.com/guviofficial/")
 wait = WebDriverWait(driver, 10)
+#driver.quit()
 # Find the followers and following elements using explicit wait
-followers_element = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/section/main/div/header/section/ul/li[2]/button")))
-following_element = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/section/main/div/header/section/ul/li[3]/button" )))
-
+followers_element = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/section/main/div/header/section/div[3]/ul/li[2]/div/button/span/span")))
+following_element = wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/section/main/div/header/section/div[3]/ul/li[3]/div/button/span/span")))
 # Extract the text which contains the number of followers and following
-followers_count = followers_element.text  # Text for following
+followers_count = followers_element.text  # text for following
 following_count = following_element.text  # Text for following
 #
 print(f"Followers: {followers_count}")
 print(f"Following: {following_count}")
-
+#
 # Close the browser
 driver.quit()
